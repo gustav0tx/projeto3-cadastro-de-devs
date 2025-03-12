3136713b85a7267fda3a02314f1e37b42683bc91
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2025-03-11 23:53:02 -0300

    Update index.js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -39,6 +39,10 @@ btnCreateTec.addEventListener('click', () => {
     op2.id = 'op2'
     op3.id = 'op3'
 
+    op1.value = '0-2 anos'
+    op2.value = '3-4 anos'
+    op3.value = '5+ anos'
+
     labelOp1.for = 'op1'
     labelOp2.for = 'op2'
     labelOp3.for = 'op3'
@@ -50,10 +54,51 @@ btnCreateTec.addEventListener('click', () => {
     const btnRemove = document.createElement('button')
     btnRemove.innerText = 'Remover'
     btnRemove.addEventListener('click', () => {
-        section.remove
+        section.remove()
     })
 
     sectionRadio.append( paraOp , op1 , labelOp1 , op2 , labelOp2 , op3 , labelOp3)
     section.append(labelTec , inputTec , breakLine , sectionRadio , breakLine , btnRemove)
     form.append(section)
+})
+
+let btnRegister = document.getElementById('devRegister')
+btnRegister.addEventListener('click', () => {
+    
+    let dev = []
+    let devName = form.children[1].value
+    dev.push(devName)
+    
+    if (form.children[1].value == '') {
+        alert('adicione o nome do Dev')
+    } else {
+        
+        if (form.childElementCount == 4) {
+            
+            console.log(`
+                Nome do Dev: ${dev[0]}
+                Tecnologias do Dev: O Dev não tem tecnologias
+                `)
+
+        } else {
+            
+            const tecnoNums = parseInt(form.childElementCount) - 4
+            
+            let allTec = []
+
+            for(let i = 0; i < tecnoNums; i++) {
+                let tecName = form.children[4 + i].children[1].value
+                let tecTime = document.querySelector('input[type="radio"]:checked').value
+                allTec.push(tecName, tecTime)
+            }
+
+            console.log(`
+                Nome do Dev: ${dev[0]}
+                Tecnologias do Dev: ${allTec}
+                `)
+
+        }
+
+    }
+
 })
